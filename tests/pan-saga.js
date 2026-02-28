fb84cc428000cb8b79325a6a4ef5a7b8db426088
Tyler Childs <email@tychi.me>
2026-02-28 01:24:49 -0800

    wip as2 saga parser 4 mqjs aka bellard compat js ffmpeg delite

---
+++ tests/pan-saga.js
@@ -0,0 +1,20 @@
+import * as url from 'node:url'
+
+if(import.meta.url.startsWith('file:')) {
+  const modulePath = url.fileURLToPath(import.meta.url);
+  if(process.argv[1] === modulePath) {
+    await main()
+  }
+}
+
+async function main() {
+  await import("./as2.js").then(({ as2 }) => {
+    const output = as2(`
+
+# Int. Bengo Apt.
+    `)
+    console.log(output)
+  })
+
+  process.exit()
+}
